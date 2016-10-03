6f0b00cf9f6959d4260886c140450d926fa35d08
jorgejbla <jorgejbla@gmail.com>
2016-10-03 18:59:39 +0200

    improved table
    improved resumes table

--- table.js
+++ table.js
@@ -4,26 +4,84 @@ var _table_ = document.createElement('table'),
     _td_ = document.createElement('td');
 
 // Builds the HTML Table out of myList json data from Ivy restful service.
- function buildHtmlTable(arr) {
+ function buildHtmlTable(arr, issubtable) {
      var table = _table_.cloneNode(false),
          columns = addAllColumnHeaders(arr, table);
      table.setAttribute("id","tableResumes");
      for (var i=0, maxi=arr.length; i < maxi; ++i) {
          var tr = _tr_.cloneNode(false);
+         var userid = "";
+
+         if (!issubtable) {
+            console.log(userid);
+            
+            var td = _td_.cloneNode(false);
+            var link = document.createElement('a');
+            var img = document.createElement('img');
+            img.setAttribute("src","img/ic_delete_black_24dp_1x.png");
+            img.setAttribute("width","24");
+            img.setAttribute("height","24");
+
+            link.setAttribute("href","#");
+            link.setAttribute("title","delete resume");
+            link.setAttribute("onclick","deleteResume('" + userid + "')");
+            link.appendChild(img);
+            td.appendChild(link);
+            tr.appendChild(td);
+
+            td = _td_.cloneNode(false);
+            link = document.createElement('a');
+            img = document.createElement('img');
+            img.setAttribute("src","img/ic_create_black_24dp_1x.png");
+            img.setAttribute("width","24");
+            img.setAttribute("height","24");
+
+            link.setAttribute("href","#");
+            link.setAttribute("title","load resume");
+            link.setAttribute("onclick","loadResume('" + userid + "')");
+            link.appendChild(img);
+            td.appendChild(link);
+            tr.appendChild(td);   
+
+            td = _td_.cloneNode(false);
+            link = document.createElement('a');
+            img = document.createElement('img');
+            img.setAttribute("src","img/ic_contacts_black_24dp_1x.png");
+            img.setAttribute("width","24");
+            img.setAttribute("height","24");
+
+            link.setAttribute("href","#");
+            link.setAttribute("title","detail resume");
+            link.setAttribute("onclick","detailResume('" + userid + "')");
+            link.appendChild(img);
+            td.appendChild(link);
+            tr.appendChild(td); 
+
+         }
+         else {
+            
+         }         
+         
          for (var j=0, maxj=columns.length; j < maxj ; ++j) {
              var td = _td_.cloneNode(false);
                  cellValue = arr[i][columns[j]];
                  if (typeof cellValue === 'object') {
+                    continue;
+                    
+                    /*
                     var listAdress = new Array();
                     listAdress.push(cellValue);
-                    
-                       var subtable = buildHtmlTable(listAdress);
+                        
+                       var subtable = buildHtmlTable(listAdress, true);
                        
                        td.appendChild(subtable);
                        tr.appendChild(td);
                        continue;
+
+                       */
                  }
                 else {
+                    
                     if( typeof cellValue === 'undefined' || cellValue === null ){
                         td.appendChild(document.createTextNode(''));
                     }
@@ -36,8 +94,9 @@ var _table_ = document.createElement('table'),
                             td.appendChild(img);
                         }
                         else {
-                            if (j === 0) {
-                                
+                            if (j === 0 && !issubtable) {
+
+                                userid = arr[i][columns[j]];
                                 var link = document.createElement('a');
                                 link.setAttribute("href","yourresume_final.html?userid="+arr[i][columns[j]]);
                                 link.setAttribute("title","view detail");
@@ -56,6 +115,10 @@ var _table_ = document.createElement('table'),
   
              tr.appendChild(td);
          }
+
+         
+         
+
          table.appendChild(tr);
      }
      return table;
@@ -68,10 +131,27 @@ var _table_ = document.createElement('table'),
  {
      var columnSet = [],
          tr = _tr_.cloneNode(false);
+
+        var th2 = _th_.cloneNode(false);
+     th2.appendChild(document.createTextNode("D"));
+     tr.appendChild(th2);
+
+        th2 = _th_.cloneNode(false);
+     th2.appendChild(document.createTextNode("L"));
+     tr.appendChild(th2);
+
+        th2 = _th_.cloneNode(false);
+     th2.appendChild(document.createTextNode("R"));
+     tr.appendChild(th2);          
+
+
      for (var i=0, l=arr.length; i < l; i++) {
          for (var key in arr[i]) {
              if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key)===-1) {
                  columnSet.push(key);
+                 if(key == "address") {
+                    continue;
+                 }
                  var th = _th_.cloneNode(false);
                  th.appendChild(document.createTextNode(key));
                  tr.appendChild(th);
